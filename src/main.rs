02f73847028efee543cd9c3658b5bda9b997721b
daemon <0xDAEF0F@proton.me>
2023-09-17 18:00:36 -0600

    checkpoint three

--- src/main.rs
+++ src/main.rs
@@ -21,22 +21,22 @@ enum DepositWithdrawalEvent {
 #[derive(Debug)]
 struct Event {
     address: Address,
-    assets: U256,
+    shares: U256,
     block_number: U64,
 }
 
 #[derive(Debug)]
 struct UserRecord {
-    assets_staked: U256,
-    rewards_per_asset_snapshot: U256,
+    shares_staked: U256,
+    rewards_per_share_snapshot: U256,
     rewards_accumulated: U256,
 }
 
 #[derive(Debug)]
 struct GlobalState<'a> {
     user_records: &'a mut HashMap<Address, UserRecord>,
-    total_staked: U256,
-    total_rewards_per_asset: U256,
+    total_shares_staked: U256,
+    total_rewards_per_share: U256,
     last_accounted_block: U64,
 }
 
@@ -51,63 +51,74 @@ impl GlobalState<'_> {
     }
 
     fn process_deposit(&mut self, evt: Event) {
-        if self.total_staked != U256::from(0) {
+        if self.total_shares_staked != U256::from(0) {
             self.update_rewards(evt.block_number)
         }
 
         if let Some(user) = self.user_records.get(&evt.address) {
+            let accrued_rewards = (self.total_rewards_per_share - user.rewards_per_share_snapshot)
+                * user.shares_staked;
+
             let user_record = UserRecord {
-                assets_staked: user.assets_staked + evt.assets,
-                rewards_accumulated: U256::from(0),
-                rewards_per_asset_snapshot: self.total_rewards_per_asset,
+                shares_staked: user.shares_staked + evt.shares,
+                rewards_accumulated: user.rewards_accumulated + accrued_rewards,
+                rewards_per_share_snapshot: self.total_rewards_per_share,
             };
+
             self.user_records.insert(evt.address, user_record);
         } else {
             self.user_records.insert(
                 evt.address,
                 UserRecord {
-                    assets_staked: evt.assets,
-                    rewards_per_asset_snapshot: self.total_rewards_per_asset,
+                    shares_staked: evt.shares,
                     rewards_accumulated: U256::from(0),
+                    rewards_per_share_snapshot: self.total_rewards_per_share,
                 },
             );
         }
+
+        self.total_shares_staked += evt.shares;
     }
 
     fn process_withdraw(&mut self, evt: Event) {
-        self.update_rewards(evt.block_number);
+        if self.total_shares_staked != U256::from(0) {
+            self.update_rewards(evt.block_number)
+        }
 
-        let record = self
+        let user_record = self
             .user_records
             .get(&evt.address)
             .expect("user should exist");
 
-        let rewards_accumulated = (self.total_rewards_per_asset
-            - record.rewards_per_asset_snapshot)
-            * record.assets_staked;
+        let rewards_accumulated = (self.total_rewards_per_share
+            - user_record.rewards_per_share_snapshot)
+            * user_record.shares_staked;
 
         self.user_records.insert(
             evt.address,
             UserRecord {
-                assets_staked: record.assets_staked - evt.assets,
-                rewards_per_asset_snapshot: self.total_rewards_per_asset,
-                rewards_accumulated: record.rewards_accumulated + rewards_accumulated,
+                shares_staked: user_record.shares_staked - evt.shares,
+                rewards_per_share_snapshot: self.total_rewards_per_share,
+                rewards_accumulated: user_record.rewards_accumulated + rewards_accumulated,
             },
         );
+
+        self.total_shares_staked -= evt.shares;
     }
 
     fn update_rewards(&mut self, block_number: U64) {
         assert!(block_number >= self.last_accounted_block);
-        assert!(self.total_staked != 0.into());
+        assert!(self.total_shares_staked != 0.into());
 
         let blocks_transcurred = U256::from((block_number - self.last_accounted_block).as_u64());
-        let rewards_per_block = U256::from(1);
+        // 1e27
+        let rewards_per_block = U256::from("1000000000000000000000000000");
         let pending_rewards: U256 = blocks_transcurred * rewards_per_block;
 
-        let pending_rewards_per_share = pending_rewards / self.total_staked;
+        let pending_rewards_per_share = pending_rewards / self.total_shares_staked;
 
         self.last_accounted_block = block_number;
-        self.total_rewards_per_asset += pending_rewards_per_share;
+        self.total_rewards_per_share += pending_rewards_per_share;
     }
 }
 
@@ -134,7 +145,7 @@ async fn main() -> Result<()> {
             DepositWithdrawalEvent::Deposit(Event {
                 address: Address::from(log.topics[2]),
                 block_number: log.block_number.unwrap(),
-                assets: U256::from(&log.data[..32]),
+                shares: U256::from(&log.data[32..]),
             })
         });
 
@@ -146,15 +157,15 @@ async fn main() -> Result<()> {
             DepositWithdrawalEvent::Withdrawal(Event {
                 address: Address::from(log.topics[3]),
                 block_number: log.block_number.unwrap(),
-                assets: U256::from(&log.data[..32]),
+                shares: U256::from(&log.data[32..]),
             })
         });
 
     let mut a: Vec<DepositWithdrawalEvent> = deposit_logs.chain(withdraw_logs).collect();
     let mut hm: HashMap<Address, UserRecord> = HashMap::new();
     let mut gs = GlobalState {
-        total_staked: U256::from(0),
-        total_rewards_per_asset: U256::from(0),
+        total_shares_staked: U256::from(0),
+        total_rewards_per_share: U256::from(0),
         last_accounted_block: U64::from(17564663),
         user_records: &mut hm,
     };
@@ -173,6 +184,8 @@ async fn main() -> Result<()> {
     });
 
     gs.process_events(a);
+
+    println!("{:?}", gs);
 
     Ok(())
 }
